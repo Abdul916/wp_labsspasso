eaaed2fbb9c8c82c90309e690b97f2cc5f856619
abdul <abdulwaheedsharif@gmail.com>
2024-12-23 16:30:27 +0500

    //

--- wp-content/themes/townhub-child/functions.php
+++ wp-content/themes/townhub-child/functions.php
@@ -1,13 +1,14 @@
 <?php
+session_start();
 /**
- * @package TownHub - Directory & Listing WordPress Theme
- * @author CTHthemes - http://themeforest.net/user/cththemes
- * @date 06-11-2019
- * @since 1.0.0
- * @version 1.0.0
- * @copyright Copyright ( C ) 2014 - 2019 cththemes.com . All rights reserved.
- * @license GNU General Public License version 3 or later; see LICENSE
- */
+* @package TownHub - Directory & Listing WordPress Theme
+* @author CTHthemes - http://themeforest.net/user/cththemes
+* @date 06-11-2019
+* @since 1.0.0
+* @version 1.0.0
+* @copyright Copyright ( C ) 2014 - 2019 cththemes.com . All rights reserved.
+* @license GNU General Public License version 3 or later; see LICENSE
+*/
 // Your php code goes here
 //
 function townhub_child_enqueue_styles() {
@@ -30,8 +31,8 @@ add_filter( 'cth_listing_dashboard_skip_content', function($dashboard){
 } );
 add_action( 'cth_listing_dashboard_content_switch', function($dashboard){
     if( $dashboard == 'affiliate-area' ) {
-        echo get_the_content(null, false, 9903); // 9903 is your page id
-    }
+echo get_the_content(null, false, 9903); // 9903 is your page id
+}
 } );
 
 
@@ -44,15 +45,14 @@ add_filter( 'cth_listing_dashboard_skip_content', function($dashboard){
 } );
 add_action( 'cth_listing_dashboard_content_switch', function($dashboard){
     if( $dashboard == 'my-account' ) {
-        echo get_the_content(null, false, 25); // 25 is your page id
-    }
+echo get_the_content(null, false, 25); // 25 is your page id
+}
 } );
 
 
 // New code by explore logics restrict plans
 add_action('woocommerce_add_to_cart_validation', 'restrict_subscription_plan_purchases', 10, 2);
 function restrict_subscription_plan_purchases($passed, $product_id) {
-    session_start();
     $user_id = get_current_user_id();
     if (!$user_id) {
         return $passed;
@@ -69,27 +69,30 @@ function restrict_subscription_plan_purchases($passed, $product_id) {
         'orderby'  => 'date',
         'order'    => 'DESC',
     ];
+
     $orders = wc_get_orders($args);
     foreach ($orders as $order) {
         foreach ($order->get_items() as $item) {
+            $membership_id = $order->get_id();
+            $_SESSION['membership_id'] = $membership_id;
             $product_name = $item->get_name();
             $order_product_id = $item->get_product_id();
             if ($order_product_id == $free_plan_id) {
-                $_SESSION['has_free'] = true;
+                $_SESSION['has_membership'] = true;
                 $_SESSION['notice_msg'] = 'You already have an active membership. Please cancel it first to buy a new plan.';
-                wp_safe_redirect( site_url( '/pricing-tables/' ) );
+                wp_safe_redirect(site_url('/pricing-tables/'));
                 exit;
             }
             if ($order_product_id == $monthly_plan_id) {
-                $_SESSION['has_monthly'] = true;
+                $_SESSION['has_membership'] = true;
                 $_SESSION['notice_msg'] = 'You have already an active membership. Please cancel that first to upgrade.';
-                wp_safe_redirect( site_url( '/pricing-tables/' ) );
+                wp_safe_redirect(site_url('/pricing-tables/'));
                 exit;
             }
             if ($order_product_id == $yearly_plan_id) {
-                $_SESSION['has_yearly'] = true;
+                $_SESSION['has_membership'] = true;
                 $_SESSION['notice_msg'] = 'You already have an active membership. Please cancel that first to downgrade.';
-                wp_safe_redirect( site_url( '/pricing-tables/' ) );
+                wp_safe_redirect(site_url('/pricing-tables/'));
                 exit;
             }
         }
@@ -97,9 +100,25 @@ function restrict_subscription_plan_purchases($passed, $product_id) {
     return $passed;
 }
 
+add_action('init', 'handle_cancel_membership_request');
+function handle_cancel_membership_request() {
+    if (isset($_POST['cancel_membership'])) {
+        unset ($_SESSION['membership_id']);
+        $_SESSION['membership_cancelled'] = true;
+        $order_id = intval($_POST['cancel_membership_order_id']);
+        $order = wc_get_order($order_id);
+        if ($order && $order->get_status() === 'completed') {
+            $order->update_status('cancelled', 'Membership cancelled by the user.');
+        } else {
+            wc_add_notice('Unable to cancel the membership. Please try again.', 'error');
+        }
+        wp_safe_redirect(site_url('/pricing-tables/'));
+        exit;
+    }
+}
+
 add_action('wp_footer', 'custom_display_pop_up_notice');
 function custom_display_pop_up_notice() {
-    session_start();
     ?>
     <style>
         #custom_popup_notice {
@@ -139,49 +158,30 @@ function custom_display_pop_up_notice() {
             popup.style.display = "none";
         }
     </script>
-    <?php if (isset($_SESSION['has_free']) && $_SESSION['has_free']) {
-        unset($_SESSION['has_free']);
+    <?php if (isset($_SESSION['has_membership']) && $_SESSION['has_membership']) {
+        unset($_SESSION['has_membership']);
         ?>
         <div id="custom_popup_notice" style="display:none;">
             <div class="popup-content">
-                <p style="">
+                <p>
                     <?php
                     echo $_SESSION['notice_msg'];
                     unset ($_SESSION['notice_msg']);
                     ?>
                 </p>
-                <button onclick="closePopup()" type="button">Close</button>
+                <form method="post">
+                    <input type="hidden" name="cancel_membership_order_id" value="<?php echo $_SESSION['membership_id']; ?>">
+                    <button type="submit" name="cancel_membership" class="button">Cancel Membership</button>
+                    <button onclick="closePopup()" type="button">Close</button>
+                </form>
             </div>
         </div>
-    <?php }
-
-    if (isset($_SESSION['has_monthly']) && $_SESSION['has_monthly']) {
-        unset($_SESSION['has_monthly']);
+    <?php } elseif (isset($_SESSION['membership_cancelled'])) {
+        unset($_SESSION['membership_cancelled']);
         ?>
-        <div id="custom_popup_notice" style="display:none;">
+        <div id="custom_popup_notice">
             <div class="popup-content">
-                <p style="color: white !important;">
-                    <?php
-                    echo $_SESSION['notice_msg'];
-                    unset ($_SESSION['notice_msg']);
-                    ?>
-                </p>
-                <button onclick="closePopup()" type="button">Close</button>
-            </div>
-        </div>
-    <?php }
-
-    if (isset($_SESSION['has_yearly']) && $_SESSION['has_yearly']) {
-        unset($_SESSION['has_yearly']);
-        ?>
-        <div id="custom_popup_notice" style="display:none;">
-            <div class="popup-content">
-                <p style="color: white !important;">
-                    <?php
-                    echo $_SESSION['notice_msg'];
-                    unset ($_SESSION['notice_msg']);
-                    ?>
-                </p>
+                <p>Your membership has been cancelled.</p>
                 <button onclick="closePopup()" type="button">Close</button>
             </div>
         </div>
